f00adf4f9ca9c82d1e4b5a5b46f264d9e3ed427d
Wancharle S Quirino <wancharle@gmail.com>
2014-10-04 18:13:09 +0000

    ajustando dados

--- savweb/savweb/admin.py
+++ savweb/savweb/admin.py
@@ -9,7 +9,10 @@ from .models import Professor, Atividade
 
 class AtividadeAdmin(admin.ModelAdmin):
     date_hierarchy = 'data'
-    list_display = ('identificacao', 'tipo', 'professor', 'data','horario_inicio', 'horario_fim', 'gps', 'numero_de_presentes', 'numero_de_participantes')
+    list_display = ('local','tipo', 'codigo_aula', 'professor', 'data','horario_inicio', 'horario_fim', 'gps', 'numero_de_presentes', 'numero_de_participantes')
+    #exclude= ("gps", "horario_fim_registrado","horario_inicio_registrado", "numero_de_presentes","numero_de_participantes")
+    fields = ('professor',('tipo','codigo_aula'),'local', 'gerencia', 'data',('horario_inicio','horario_fim'))
+    
 admin.site.register(Professor)
 admin.site.register(Atividade,AtividadeAdmin)
 
--- savweb/savweb/models.py
+++ savweb/savweb/models.py
@@ -13,30 +13,33 @@ class Professor(models.Model):
         return "{0}".format(self.user.username)
     
 class Atividade(models.Model):
-    TIPO_EXPEDIENTE = 'EX'
+    #TIPO_EXPEDIENTE = 'EX'
     TIPO_ALMOCO = 'AL'
     TIPO_AULA = 'AU'
-    TIPOS = ((TIPO_EXPEDIENTE, u"Expediente"), (TIPO_ALMOCO, u"Almoço"), (TIPO_AULA, u"Aula"))
+    TIPOS = ((TIPO_ALMOCO, u"Almoço"), (TIPO_AULA, u"Aula"))
 
+    tipo  = models.CharField(max_length=2, default=TIPO_AULA,choices= TIPOS)
+    codigo_aula = models.IntegerField(null=True,default=0,blank=True, verbose_name=u"Código Aula")
+    local = models.CharField(max_length=150)
     professor = models.ForeignKey(Professor)
-    tipo  = models.CharField(max_length=2, choices= TIPOS)
-    
-    identificacao = models.CharField(max_length=50, verbose_name=u"identificação", help_text=u"identificação da atividade. Exemplo: codigo de atividade - nome da turma")
+    gerencia = models.CharField(max_length=100, verbose_name=u"gerência")
     numero_de_presentes = models.IntegerField(default=0,null=True,blank=True)
     numero_de_participantes = models.IntegerField(default=0,null=True, blank=True)
 
     data = models.DateField()
-    horario_inicio = models.TimeField()
-    horario_fim = models.TimeField()
+    horario_inicio = models.TimeField(verbose_name=u"horário início")
+    horario_inicio_registrado = models.TimeField(null=True,blank=True)
+    horario_fim = models.TimeField(verbose_name=u"horário fim")
+    horario_fim_registrado = models.TimeField(null=True,blank=True)
 
-    gps = models.CharField(max_length=30,help_text=u"Coordenadas do gps no formato <latitude, longitude>. Exemplo: -43.004579, 25.445676" )
+    gps = models.CharField(null=True, blank=True,max_length=30,help_text=u"Coordenadas do gps no formato <latitude, longitude>. Exemplo: -43.004579, 25.445676" )
 
     def __str__(self):
-        return "{0}".format(self.identificacao)
+        return "{0}".format(self.codigo_aula)
 
     def clean(self):
-        if self.tipo == Atividade.TIPO_AULA and  (self.numero_de_presentes == 0 or self.numero_de_participantes == 0):
-            raise ValidationError(u"é preciso informar o numero de pessoas presentes e participantes da atividade")
+       if self.tipo == Atividade.TIPO_AULA and (self.codigo_aula == None  or self.codigo_aula == 0) :
+            raise ValidationError(u"Informe o código da aula para atividades do 'tipo aula'.")
 
         
 # vim: set ts=4 sw=4 sts=4 expandtab: 
